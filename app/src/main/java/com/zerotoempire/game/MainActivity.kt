128404dc482f25d41a790cd630bab4db74e0ebcb
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-08-14 17:05:39 +0200

    audio: attach premium SFX engine to activity lifecycle

--- app/src/main/java/com/zerotoempire/game/MainActivity.kt
+++ app/src/main/java/com/zerotoempire/game/MainActivity.kt
@@ -7,6 +7,12 @@ import androidx.activity.compose.setContent
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        GameSfxBus.attach(PremiumSfxEngine(applicationContext))
         setContent { GrowthRuntimeRoot() }
+    }
+
+    override fun onDestroy() {
+        GameSfxBus.detach()
+        super.onDestroy()
     }
 }
