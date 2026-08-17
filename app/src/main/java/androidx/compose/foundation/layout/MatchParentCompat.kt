cd22900a6d18306e0605edf0da38df43824d9843
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-08-17 21:53:28 +0200

    cleanup: remove obsolete layout compatibility shim

--- app/src/main/java/androidx/compose/foundation/layout/MatchParentCompat.kt
+++
@@ -1,6 +0,0 @@
-package androidx.compose.foundation.layout
-
-import androidx.compose.ui.Modifier
-
-/** Compatibility shim for the Compose version pinned by the project. */
-fun Modifier.matchParentSize(): Modifier = fillMaxSize()
